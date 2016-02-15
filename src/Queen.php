87891630531b6141dc2313bb69531b7b748d2fcb
Jon Nardine and Ryan Brown <browneryan87@gmail.com>
2016-02-15 13:24:32 -0800

    add chessboard image and refactor canAttack

--- src/Queen.php
+++ src/Queen.php
@@ -4,11 +4,7 @@ class Queen
 {
     function canAttack($queen_x, $queen_y, $king_x, $king_y)
     {
-        if($queen_x === $king_x){
-            return true;
-        } else {
-        return (abs($queen_x - $king_x) === abs($queen_y - $king_y));
-        }
+        return ((($queen_x === $king_x) || ($queen_y === $king_y)) || ((abs($queen_x - $king_x) === abs($queen_y - $king_y))));
     }
 }
 
--- tests/QueenTest.php
+++ tests/QueenTest.php
@@ -50,6 +50,21 @@ class QueenTest extends PHPUnit_Framework_TestCase
         //Assert
         $this->assertEquals(true, $result);
     }
+    function test_canAttack_horizontalAttack()
+    {
+        //Arrange
+        $test_Queen = new Queen;
+        $input1 = 3;
+        $input2 = 7;
+        $input3 = 4;
+        $input4 = 7;
+
+        //Act
+        $result = $test_Queen->canAttack($input1, $input2, $input3, $input4);
+
+        //Assert
+        $this->assertEquals(true, $result);
+    }
 }
 
 ?>
